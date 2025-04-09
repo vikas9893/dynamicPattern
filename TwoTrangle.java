cdd0dd3cd0110a9ebd1f75851059595bfddd4735
Vikas Maurya <146058887+vikas9893@users.noreply.github.com>
2025-04-09 09:28:22 +0530

    Add files via upload

---
+++ TwoTrangle.java
@@ -0,0 +1,25 @@
+package dynamicpattern;
+
+import java.util.Scanner;
+
+public class TwoTrangle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:- ");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <= 2*(n-i)-1; j++) {
+				System.out.print("  ");
+			}
+			for (int j = 1; j <= i; j++) {
+				if(j<n) {
+					System.out.print("* ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
